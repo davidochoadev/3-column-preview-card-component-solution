6d14cc9913a3d019d2ee5f176a954a1641006232
davideochoaa <davideochoaa@gmail.com>
2023-06-05 20:33:56 +0200

    ✅ Feat: Automatic Console Messages

--- scripts/git.js
+++ scripts/git.js
@@ -6,25 +6,30 @@ const git = simpleGit();
 async function autoCommitFunc() {
    const message = process.argv.slice(4).join(" ") || "General Edits on Current Branch";
    let messagePrefix;
+   let consoleMessage;
 
    switch(process.argv.slice(2)[0]){
       case 'fix':
          messagePrefix = "🛠 Fix";
+         consoleMessage = "🛠 Fixing Commit Successful";
          break;
       case 'feat':
          messagePrefix = "✅ Feat";
+         consoleMessage = "✅ Featuring Commit Successful";
          break;
       case 'refactoring':
          messagePrefix = "✏️ Refactoring";
+         consoleMessage = "✏️ Refactoring Commit Successful";
          break;
       default:
-         messagePrefix = "🤖 Automatic commit";
+         messagePrefix = "🤖 Automatic Commit";
+         consoleMessage = "🤖 Automaitc Commit Successful";
    }
 
    await git.add(".");
    await git.commit(`${messagePrefix}: ${message}`);
    await git.push("origin", "main");
-   console.log(chalk.blue('Commit Successful'));
+   console.log(chalk.blue(consoleMessage));
 }
 
 autoCommitFunc();
