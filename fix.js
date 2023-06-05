65a36c120060d1bea3b958b4bb31df6c61e59058
davideochoaa <davideochoaa@gmail.com>
2023-06-05 16:11:42 +0200

    ⚙️Fix: fixed component

---
+++ fix.js
@@ -0,0 +1,11 @@
+const simpleGit = require("simple-git");
+const git = simpleGit.default();
+
+async function main(message) {
+   await git.add(".");
+   await git.commit(`⚙️Fix: ${message}`);
+   await git.push(origin, main);
+}
+
+// logs "test-branch"
+main("fixed component");
