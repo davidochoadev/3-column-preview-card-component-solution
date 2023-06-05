7fb57a84a95a020d702f3072ca50d3966bd9057f
davideochoaa <davideochoaa@gmail.com>
2023-06-05 16:21:36 +0200

    ⚙️Fix: personal messageee

--- scripts/fix.js
+++ scripts/fix.js
@@ -2,7 +2,7 @@ const simpleGit = require("simple-git");
 const git = simpleGit.default();
 
 async function main() {
-   const message = process.argv[2] || "Fixing current branch";
+   const message = process.argv.slice(3).join(" ") || "Fixing current branch";
    await git.add(".");
    await git.commit(`⚙️Fix: ${message}`);
    await git.push("origin","main");
