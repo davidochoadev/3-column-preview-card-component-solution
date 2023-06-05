a5a15baeb22bc141f0bc91a8f41972842de893e2
davideochoaa <davideochoaa@gmail.com>
2023-06-05 22:03:28 +0200

    🛠 Fix: Removed node package manager

--- scripts/git.js
+++ scripts/git.js
@@ -1,35 +1,36 @@
-import chalk from 'chalk';
-import simpleGit from 'simple-git';
+import chalk from "chalk";
+import simpleGit from "simple-git";
 
 const git = simpleGit();
 
 async function autoCommitFunc() {
-   const message = process.argv.slice(4).join(" ") || "General Edits on Current Branch";
-   let messagePrefix;
-   let consoleMessage;
+  const message =
+    process.argv.slice(4).join(" ") || "General Edits on Current Branch";
+  let messagePrefix;
+  let consoleMessage;
 
-   switch(process.argv.slice(2)[0]){
-      case 'fix':
-         messagePrefix = "🛠 Fix";
-         consoleMessage = "🛠 Fixing Commit Successful";
-         break;
-      case 'feat':
-         messagePrefix = "✅ Feat";
-         consoleMessage = "✅ Featuring Commit Successful";
-         break;
-      case 'refactoring':
-         messagePrefix = "✏️ Refactoring";
-         consoleMessage = "✏️ Refactoring Commit Successful";
-         break;
-      default:
-         messagePrefix = "🤖 Automatic Commit";
-         consoleMessage = "🤖 Automaitc Commit Successful";
-   }
+  switch (process.argv.slice(2)[0]) {
+    case "fix":
+      messagePrefix = "🛠 Fix";
+      consoleMessage = "🛠 Fixing Commit Successful";
+      break;
+    case "feat":
+      messagePrefix = "✅ Feat";
+      consoleMessage = "✅ Featuring Commit Successful";
+      break;
+    case "refactoring":
+      messagePrefix = "✏️ Refactoring";
+      consoleMessage = "✏️ Refactoring Commit Successful";
+      break;
+    default:
+      messagePrefix = "🤖 Automatic Commit";
+      consoleMessage = "🤖 Automaitc Commit Successful";
+  }
 
-   await git.add(".");
-   await git.commit(`${messagePrefix}: ${message}`);
-   await git.push("origin", "main");
-   console.log(chalk.green(consoleMessage));
+  await git.add(".");
+  await git.commit(`${messagePrefix}: ${message}`);
+  await git.push("origin", "main");
+  console.log(chalk.green(consoleMessage));
 }
 
 autoCommitFunc();
